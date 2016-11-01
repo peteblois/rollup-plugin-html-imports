bb266ad48341f7ee2b64c2a293f1dcfd588b0e83
Pete Blois <pete@blois.us>
2016-10-31 23:32:25 -0700

    SOmething.

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import buble from 'rollup-plugin-buble';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 var pkg = require('./package.json');
 
 export default {
   entry: 'src/index.js',
   plugins: [
+    //nodeResolve({ jsnext: true }),
     buble({
       transforms: {
         dangerousForOf: true
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import {createFilter} from 'rollup-pluginutils';
 import parse5 from 'parse5';
 import dom5 from 'dom5';
 import path from 'path';
+import MagicString from 'magic-string';
 
 function transformHtmlToJs(htmlDocument, htmlPath) {
+  const magicString = new MagicString(htmlDocument);
+
   const ast = parse5.parseFragment(htmlDocument);
   const basePath = path.dirname(htmlPath);
 
@@ -13,23 +16,40 @@ function transformHtmlToJs(htmlDocument, htmlPath) {
   for (const htmlImport of htmlImports) {
     const href = dom5.getAttribute(htmlImport, 'href');
     const importPath = path.resolve(basePath, href);
-    const line = `import htmlImport${jsImports.length} from '${importPath}';\n`;
+    const line = `import htmlImport${jsImports.length} from './${href}';`;
     jsImports.push(line);
 
     dom5.remove(htmlImport);
   }
 
-
+  let html = JSON.stringify(parse5.serialize(ast));
+  html = html.replace(/\\n/g, '\n');
+  html = html.replace(/\`/g, '\\`');
   const jsDocument = `
-${jsImports}
+${jsImports.join('\n')}
 
-const html = ${JSON.stringify(parse5.serialize(ast))};
+const html = \`${html}\`;
 
 const range = document.createRange();
-export default range.createContextualFragment(html);
+const fragment = range.createContextualFragment(html);
+
+const link = document.createElement('link');
+link.appendChild(fragment);
+document.head.appendChild(link);
+
+export default fragment;
 `;
-  console.log(jsDocument);
-  return jsDocument;
+
+  magicString.slice(0);
+  magicString.append(jsDocument);
+
+  // magicString.overwrite(0, -1, jsDocument);
+
+  // console.log(jsDocument);
+
+  return {code: magicString.toString(), map: magicString.generateMap({hires: true})};
+
+  // return jsDocument;
 }
 
 const isHtmlImportNode = dom5.predicates.AND(
@@ -50,10 +70,7 @@ export default function htmlImport(opts = {}) {
 
     transform(code, id) {
       if (filter(id)) {
-        return {
-          code: transformHtmlToJs(code, id),
-          map: { mappings: '' }
-        };
+        return transformHtmlToJs(code, id);
       }
     }
   };
